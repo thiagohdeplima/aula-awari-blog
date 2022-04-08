2ef1362ab3174805732a0e945948fbacd57ba927
Thiago Henrique <fale@thiago.tech>
2022-04-07 21:02:17 -0300

    feat: use response.json instead a string

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var express = require("express");
 var server = express();
 
 server.get("/posts", function(request, response) {
-    response.header("content-type", "application/json");
-    response.send('{"message": "Hello World"}');
+    response.json({message: "Hello World"})
 });
 
 server.listen(3000);
