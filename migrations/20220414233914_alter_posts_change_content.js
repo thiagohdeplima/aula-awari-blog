defce399d79d974ecfa8d9e2be3062195ee23c1b
Thiago Henrique <fale@thiago.tech>
2022-04-14 20:50:40 -0300

    chore: add a comment

--- migrations/20220414233914_alter_posts_change_content.js
+++ migrations/20220414233914_alter_posts_change_content.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function(knex) { // TODO: How to run all the migrations once?
     return knex.schema.
         alterTable('posts', function(table) {
             table.text('content').notNullable().alter()
