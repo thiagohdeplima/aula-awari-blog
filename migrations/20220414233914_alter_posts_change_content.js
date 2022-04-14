54353059c67f824c6b779e6287692947ab394ec1
Thiago Henrique <fale@thiago.tech>
2022-04-14 20:50:02 -0300

    chore: alter table posts to change content type

---
+++ migrations/20220414233914_alter_posts_change_content.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.
+        alterTable('posts', function(table) {
+            table.text('content').notNullable().alter()
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.
+        alterTable('posts', function(table) {
+            table.string('content', 255).notNullable().alter()
+        })
+};
